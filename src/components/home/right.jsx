8901dc2bf8325bd14326438900ee300cb234e7b8
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2023-02-05 07:01:36 +0530

    userlist fixed

--- src/components/home/right.jsx
+++ src/components/home/right.jsx
@@ -1,7 +1,9 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import { NotificationContainer, NotificationLabel, Notification, NotifUserImage, NotifContent, NotifUserName, NotifMessage, UserListLabel, TotalUserCount, TotalUsersInfoContainer, UsersListContainer, UserRow, UserImageForList, UserDetails, UserName2, ShortDesc2, FollowButton, User, SearchContainer, SearchIcon, FontAwesomeSearch, SearchInput, UsersContainer, NoUserContainer } from './right.styled';
+import { getAllUsers } from "../../apis/users";
+import { Avatars, getSrc } from "../../constant/avatarResolver";
 
 
 const Notifications = [
@@ -47,69 +49,83 @@ const Notifications = [
     },
 ]
 
-const Users = [
-    {
-        id: 1,
-        avator: null,
-        name: "John Doe",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 2,
-        avator: null,
-        name: "Omkar",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 3,
-        avator: null,
-        name: "Raunak",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 4,
-        avator: null,
-        name: "abhishek",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 5,
-        avator: null,
-        name: "Hi",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 6,
-        avator: null,
-        name: "Hello",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 7,
-        avator: null,
-        name: "Nice",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 8,
-        avator: null,
-        name: "Good",
-        shortDesc: "Famous Scientist"
-    },
-    {
-        id: 9,
-        avator: null,
-        name: "Spit",
-        shortDesc: "Famous Scientist"
-    },
-]
+// const Users = [
+//     {
+//         id: 1,
+//         avator: null,
+//         name: "John Doe",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 2,
+//         avator: null,
+//         name: "Omkar",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 3,
+//         avator: null,
+//         name: "Raunak",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 4,
+//         avator: null,
+//         name: "abhishek",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 5,
+//         avator: null,
+//         name: "Hi",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 6,
+//         avator: null,
+//         name: "Hello",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 7,
+//         avator: null,
+//         name: "Nice",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 8,
+//         avator: null,
+//         name: "Good",
+//         shortDesc: "Famous Scientist"
+//     },
+//     {
+//         id: 9,
+//         avator: null,
+//         name: "Spit",
+//         shortDesc: "Famous Scientist"
+//     },
+// ]
 
 const Right = () => {
 
     const [queryParam, setQueryParam] = useState('');
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    
+    const filterAfterGettingUsers = () => {
+            const res = users.filter(user => user.name.toLowerCase().includes(queryParam.toLowerCase()));
+            setFilteredUsers(res);
+    }
 
-    const filteredUsers = Users.filter(user => user.name.toLowerCase().includes(queryParam.toLowerCase()));
-
+    useEffect(() => {
+      getAllUsers()
+        .then(users => {
+            setUsers(users);
+            filterAfterGettingUsers();
+            console.log("All users:", users);
+        })
+    }, [])
+    
 
     return (
         <>
@@ -135,21 +151,21 @@ const Right = () => {
 
             <UsersContainer>
                 <SearchContainer>
-                    <SearchInput type="text" value={queryParam} onChange={e => setQueryParam(e.target.value)} id="searchInput" placeholder="Search User"></SearchInput>
+                    <SearchInput type="text" value={queryParam} onChange={e => {setQueryParam(e.target.value);filterAfterGettingUsers()}} id="searchInput" placeholder="Search User"></SearchInput>
                     <SearchIcon>
                         <FontAwesomeSearch icon={faSearch} />
                     </SearchIcon>
                 </SearchContainer>
                 <UsersListContainer>
                     {filteredUsers.length > 0
-                        ? filteredUsers.map((user) => {
+                        ? filteredUsers.map((user, index) => {
                             return (
-                                <UserRow key={user.id}>
+                                <UserRow key={index}>
                                     <User>
-                                        <UserImageForList />
+                                        <UserImageForList src={getSrc(user?.gender, user?.avatar)}></UserImageForList>
                                         <UserDetails>
-                                            <UserName2>{user.name}</UserName2>
-                                            <ShortDesc2>{user.shortDesc}</ShortDesc2>
+                                            <UserName2>{`${user.fname} ${user.lname}`}</UserName2>
+                                            <ShortDesc2>{user['bio']}</ShortDesc2>
                                         </UserDetails>
                                     </User>
                                     <FollowButton onClick={() => console.log('click')}>Follow</FollowButton>
@@ -157,7 +173,7 @@ const Right = () => {
                             );
                         })
                         : filteredUsers.length === 0 && queryParam.length > 0 ? <NoUserContainer>Sorry no user found :(</NoUserContainer>
-                            : Users.map((user) => {
+                            : users.map((user) => {
                                 return (
                                     <UserRow key={user.id}>
                                         <User>
